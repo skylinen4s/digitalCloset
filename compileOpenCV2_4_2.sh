705889f37fddc473b157c122f04176b6df6960c1
abkyes <abkabkabkyes@gmail.com>
2013-10-31 16:03:06 +0800

    write compiler's argumant  by hardcode

--- compileOpenCV2_4_2.sh
+++ compileOpenCV2_4_2.sh
@@ -2,4 +2,4 @@
 export LD_LIBRARY_PATH=/usr/lib
 echo `pkg-config --cflags opencv`
 echo `pkg-config --libs opencv`
-g++ `pkg-config --cflags opencv` -g -o $2 $1 `pkg-config --libs opencv`
+g++ `pkg-config --cflags opencv` -g -o facedetect facedetect.cpp digitalCloset_Motorcontrol.c  `pkg-config --libs opencv`
