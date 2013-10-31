0f162e22bff1b215c4ae7b06f37b7cc195b094e7
abkyes <abkabkabkyes@gmail.com>
2013-11-01 02:52:59 +0800

    add motor control header file

---
+++ digitalCloset_Motorcontrol.h
@@ -0,0 +1,8 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+
+void motor_control(int controlTime, int motorNumber, int orient);
+void motor_init();
